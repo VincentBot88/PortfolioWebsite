66f74f9e93aa63a0b412d0222502320efeb6a3f1
VincentBot88 <vincent.cheng22@ycdsbk12.ca>
2024-09-09 23:24:46 -0400

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
