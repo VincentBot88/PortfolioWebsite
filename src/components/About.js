1f3490650e1ce14e31e0dfffeccc06b46c43c643
VincentBot88 <vincent.cheng22@ycdsbk12.ca>
2024-09-18 19:15:01 -0400

    resume update

--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,7 @@ const About = () => {
             </a>
           </li>
           <li>
-            <a href="https://docs.google.com/document/d/10MQg21yuvH1zvps0jC7fUQscR45kmZjd38pIpIC47qU/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+            <a href="assets/resume.pdf" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faFile} /> Resume
             </a>
           </li>
